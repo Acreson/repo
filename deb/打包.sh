9ab200b971c64c63a77e9f413b0eeb924c30b141
他是个疯子 <tashigefengzi@tashigefengzis-iMac.local>
2019-04-22 20:44:21 +0800

    .

--- deb/打包.sh
+++ deb/打包.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 find . -name '*.DS_Store' -type f -delete
-dpkg-deb -b a/ /Users/tashigefengzi/Desktop/github/教程/debs/
-rm -r -f /Users/tashigefengzi/Desktop/github/教程/deb/a.deb
+dpkg-deb -b a/ /Users/tashigefengzi/Desktop/github/repo/debs/
+rm -r -f /Users/tashigefengzi/Desktop/github/repo/deb/a.deb
 
 
 

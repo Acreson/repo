ef253f1a770e34fd2afa2cc51048d757413c8981
Crazyhiphop <hiphop@HipHopdeiMac.local>
2019-05-04 04:55:47 +0800

    1

--- 删掉bz2重新生成bz2.sh
+++ 删掉bz2重新生成bz2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 dpkg-scanpackages -m ./debs > Packages
-rm -r -f /Users/hiphop/Desktop/repo/教程/Packages.bz2
+rm -r -f /Users/hiphop/Desktop/repo/Packages.bz2
 bzip2 Packages
 dpkg-scanpackages -m ./debs > Packages

ba3c1d79d81d093d7fe24faf63bb269252299c13
Crazyhiphop <hiphop@HipHopdeiMac.local>
2019-05-04 04:28:19 +0800

    1

--- deb/打包.sh
+++ deb/打包.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 find . -name '*.DS_Store' -type f -delete
-dpkg-deb -b a/ /Users/tashigefengzi/Desktop/github/repo/debs/
-rm -r -f /Users/tashigefengzi/Desktop/github/repo/deb/a.deb
+dpkg-deb -b a/ /Users/hiphop/Desktop/repo/debs/
+rm -r -f /Users/hiphop/Desktop/repo/deb/a.deb
 
 
 
--- 删掉bz2重新生成bz2.sh
+++ 删掉bz2重新生成bz2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 dpkg-scanpackages -m ./debs > Packages
-rm -r -f /Users/tashigefengzi/Desktop/github/教程/Packages.bz2
+rm -r -f /Users/hiphop/Desktop/repo/教程/Packages.bz2
 bzip2 Packages
 dpkg-scanpackages -m ./debs > Packages
